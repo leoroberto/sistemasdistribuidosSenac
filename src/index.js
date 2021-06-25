650aec045727a4b497c1d581b33ab3d128273715
Leonardo Farias <leoroberto@gmail.com>
2021-06-25 19:41:24 -0300

    Rota Leo adicionada

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ app.get('/', (req, res ) =>
     res.json({ message: 'Docker ér complicado de se entender se vc não visualiza bem! Docker is easy 🐳 Nada se cria tudo se copia' }) 
 );
 
+app.get('/leo', (req, res ) => 
+    res.json({ message: 'Você acessou a url Leo' }) 
+);
+
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`app listening on http://localhost:${port}`) );
